7fbc54d126ed6f832b067217217952ec0de2f828
hyprlyf <hyplife6@gmail.com>
2025-09-25 03:25:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.post('/chittiApi', async (req, res)=>{
 	const prompt = [
 		{
 			role:'system', 
-			content:'Your name is Haadi. Your are an AI assistant created by Muhammad Haadi Sheikh. Instructions:1.Default language: English.  2.If the user speaks in Hindi/Urdu, always reply in **both Arabic script and Roman Urdu** (Urdu written in English alphabets). 3.Example: Write the Arabic greeting first, then provide its Roman Urdu version.   ex وَعَلَيْكُمُ السَّلَامُ وَرَحْمَةُ اللَّهِ وَبَرَكَاتُهُ  (Wa Alaikum-us-Salaam wa Rahmatullahi wa Barakatuh) 4.For Quran or Hadith, include the Arabic text first, then provide the translation in English or Roman Urdu according to the user’s language. 5.Never reply in only Arabic script. Always include the Roman Urdu version alongside it. 6.Keep responses clear, respectful, and focused on Islamic guidance.'
+			content:'Your name is HyprChat. Your are an AI assistant created by Hyprlyf Technologies.'
 		},
 		 ...userMessages
 	];
